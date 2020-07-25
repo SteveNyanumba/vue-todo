f3e5627fa80d8f77988547a53ecba084c7423d39
Steve Nyanumba <stevennmb9@gmail.com>
2020-07-26 02:24:34 +0300

    Added Login Logiv

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,39 @@
 const User = require('../app/User')
 const router = require('express').Router()
 
-router.post('/login',(req,res)=>{
+//middleware
+const auth = (req,res,next)=>{
+    if(req.session.user && req.cookies.user_sid){
+        res.redirect('/todos')
+    }else{
+        next()
+    }
+}
+
+router.post('/login', (req,res)=>{
     const {username, password} = req.body
 
+    User.findOne({
+        where:{
+            username,
+        }
+    })
+    .then((user) => {
+        if(!user){
+            res.status(404).json({
+                message: 'User not found'
+            })
+        }else if(!user.ValidPassword(password)){
+            res.status(400).json({
+                message: 'Password is invalid'
+            })
+        }else{
+            req.session.user = user.dataValues;
+            res.redirect('/todos');
+        }
+    }).catch((err) => {
+        
+    });
 })
 
 
--- server.js
+++ server.js
@@ -3,14 +3,40 @@ const app = express()
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const apiRoutes = require('./routes/api')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 dotenv.config({
     path:'./.env'
 })
-const {PORT, DB_CONNECTION, NODE_ENV} = process.env
+const {PORT, DB_CONNECTION, NODE_ENV, APP_SECRET} = process.env
 if(NODE_ENV === 'development'){
     morgan('dev')
 }
+
+app.use(bodyParser.json())
+
+app.use(cookieParser())
+
+app.use(session({
+    key:'user_sid',
+    secret: APP_SECRET,
+    resave:false,
+    saveUninitialized: false,
+    cookie:{
+        expires: 300000
+    }
+}))
+
+//clear cookies if session expires
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
+});
+
 
 app.use(express.static('./public'))
 
