2b8d0fafb30944b27267727f4076560e43f94889
Steve Nyanumba <stevennmb9@gmail.com>
2020-07-26 02:00:36 +0300

    Added Models and Routes files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# vue-todo
+# Vue Todo App
+
+This is a show case application using express js on the backend and vue JS on the frontend.
+
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
+const User = require('../app/User')
 const router = require('express').Router()
 
+router.post('/login',(req,res)=>{
+    const {username, password} = req.body
 
+})
 
 
 
